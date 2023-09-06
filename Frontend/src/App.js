e15c969b2f01d1054cb0e316d40b40bc5bd79136
prakash samanatartay <psamantaray77@gmail.com>
2023-09-06 07:37:05 +0530

    init

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,40 +1,16 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Sidebar from './components/sidebar/SideNavbar';
-import Dashboard from './components/pages/Dashboard';
-import WorkManagement from './components/pages/WorkManagement';
-import General from './components/pages/General/General';
-import DepartmentManagement from './components/pages/General/DepartmentManagement';
-import EmployeeManagement from './components/pages/General/EmployeeManagement';
-import RoleManagement from './components/pages/General/RoleManagement';
-import Attendance from './components/pages/Attendance';
-import WorkAnalysis from './components/pages/WorkAnalysis';
-import Payroll from './components/pages/Payroll';
-import './App.css';
-
-
-
-const App = () => {
+import React from "react";
+import styled from "styled-components";
+import Dashboard from "./components/Dashboard";
+import Sidebar from "./components/Sidebar";
+export default function App() {
   return (
-    <Router>
-      <div className="app">
-        <Sidebar />
-        <main>
-          <Switch>
-            <Route path="/dashboard" component={Dashboard} />
-            <Route path="/work-management" component={WorkManagement} />
-            <Route path="/general" exact component={General} />
-            <Route path="/general/department-management" component={DepartmentManagement} />
-            <Route path="/general/employee-management" component={EmployeeManagement} />
-            <Route path="/general/role-management" component={RoleManagement} />
-            <Route path="/attendance" component={Attendance} />
-            <Route path="/work-analysis" component={WorkAnalysis} />
-            <Route path="/payroll" component={Payroll} />
-          </Switch>
-        </main>
-      </div>
-    </Router>
+    <Div>
+      <Sidebar />
+      <Dashboard />
+    </Div>
   );
-};
+}
 
-export default App;
+const Div = styled.div`
+  position: relative;
+`;
